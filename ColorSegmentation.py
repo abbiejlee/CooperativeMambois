db22ae9079b1e3a29f9cc6908baf1f591dc367e3
tsun34 <tingxiao@mit.edu>
2018-11-26 20:37:01 -0500

    add color segmentation algorithm & its test file

---
+++ ColorSegmentation.py
@@ -0,0 +1,57 @@
+import cv2
+import imutils
+import numpy as np
+import pdb
+
+def cd_color_segmentation(img):
+    """
+    Implement the cone detection using color segmentation algorithm
+    Input:
+        img: np.3darray; the input image with a cone to be detected
+    Return:
+        bbox: ((x1, y1), (x2, y2)); the bounding box of the cone, unit in px
+                (x1, y1) is the bottom left of the bbox and (x2, y2) is the top right of the bbox
+    """
+    # Find bounding box coordinates
+    # x1 = 0
+    # y1 = 0
+    # x2 = 0
+    # y2 = 0
+    LOW_HUE = 3
+    HIGH_HUE = 27
+    LOW_SATURATION = 159
+    HIGH_SATURATION = 255
+    LOW_VALUE = 160
+    HIGH_VALUE = 255
+
+    cone_img = img.copy()
+     
+    #convert to hsv
+    hsv = cv2.cvtColor(cone_img, cv2.COLOR_BGR2HSV)
+    h, s, v = cv2.split(hsv)
+
+    #apply color ranges
+    hue_mask = cv2.inRange(h, LOW_HUE, HIGH_HUE)
+    sat_mask = cv2.inRange(s, LOW_SATURATION, HIGH_SATURATION)
+    val_mask = cv2.inRange(v, LOW_VALUE, HIGH_VALUE)
+
+    #apply mask
+    total_mask = cv2.bitwise_and(hue_mask, cv2.bitwise_and(sat_mask, val_mask))
+    cone_img = cv2.bitwise_and(cone_img, cone_img, mask=total_mask)
+
+    #get contours
+    _, contours, hierarchy = cv2.findContours(total_mask, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+
+    #get largest contour
+    contour_areas = [cv2.contourArea(contour) for contour in contours]
+    max_ind = np.argmax(contour_areas)
+    biggest_contour = contours[max_ind]
+
+    #draw bounding rectangle
+    x1,y1,w,h = cv2.boundingRect(biggest_contour)
+    x2,y2 = (x1+w, y1+h)
+    cv2.rectangle(cone_img,(x1,y1),(x2,y2),(0,255,0),2)
+    ########### YOUR CODE ENDS HERE ###########
+
+    # Return bounding box
+    return ((x1, y1), (x2, y2))
---
+++ ColorSegmentation_test.py
@@ -0,0 +1,82 @@
+import cv2
+import numpy as np
+import csv
+import ast
+import sys
+from ColorSegmentation import cd_color_segmentation
+
+def iou_score(bbox1, bbox2):
+    """
+    Return the IoU score for the two bounding boxes
+    Input:
+        bbox1: ((x1, y1), (x2, y2)); the bounding box of the cone, unit in px
+                (x1, y1) is the top left of the bbox and (x2, y2) is the bottom right of the bbox
+        bbox2: ((x1, y1), (x2, y2)); the bounding box of the cone, unit in px
+                (x1, y1) is the top left of the bbox and (x2, y2) is the bottom right of the bbox
+    Return:
+        score: float; the IoU score
+    """
+
+    # Determine intersection rectangle
+    x_int_1 = max(bbox1[0][0], bbox2[0][0])
+    y_int_1 = max(bbox1[0][1], bbox2[0][1])
+    x_int_2 = min(bbox1[1][0], bbox2[1][0])
+    y_int_2 = min(bbox1[1][1], bbox2[1][1])
+
+    # Compute area of intersection
+    area_int = (x_int_2 - x_int_1 + 1) * (y_int_2 - y_int_1 + 1)
+
+    # Compute area of both bounding boxes
+    area_bbox1 = (bbox1[1][0] - bbox1[0][0] + 1) * (bbox1[1][1] - bbox1[0][1] + 1)
+    area_bbox2 = (bbox2[1][0] - bbox2[0][0] + 1) * (bbox2[1][1] - bbox2[0][1] + 1)
+
+    # Compute area of union
+    area_union = float(area_bbox1 + area_bbox2 - area_int)
+
+    # Compute and return IoU score
+    score = area_int / area_union
+
+    # Reject negative scores
+    if score < 0:
+        score = 0
+
+    return score
+
+def test_algorithm(detection_func, test_file_path = "./test_images.csv"):
+    """
+    Test a cone detection function and return the average score based on all the test images
+    Input:
+        detection_func: func; the cone detection function that takes the np.3darray
+                as input and return (bottom, left, top, right) as output
+        test_file_path: string; the path to the test file
+    Return:
+        scores: dict; the score for each test image
+    """
+    # Keep track of scores
+    scores = {}
+
+    # Open test images csv
+    with open(test_file_path) as csvDataFile:
+        csvReader = csv.reader(csvDataFile)
+        # Iterate through all test images
+        for row in csvReader:
+            # Find image path and ground truth bbox
+            img_path = row[0]
+            bbox_true = ast.literal_eval(row[1])
+            img = cv2.imread(img_path)
+
+            # Detection bbox
+            bbox_est = detection_func(img)
+            score = iou_score(bbox_est, bbox_true)
+            
+            # Add score to dict
+            scores[img_path] = score
+
+    # Return scores
+    return scores
+
+if __name__ == '__main__':
+    scores = test_algorithm(cd_color_segmentation)
+    if scores:
+        for (img, val) in scores.iteritems():
+            print((img, val))
