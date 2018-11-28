c01a8c762a589f6df664765ef7041d2cf9bf3f49
tsun34 <tingxiao@mit.edu>
2018-11-27 19:27:11 -0500

    Update ColorSegmentation.py

--- ColorSegmentation.py
+++ ColorSegmentation.py
@@ -5,25 +5,26 @@ import pdb
 
 def cd_color_segmentation(img):
     """
-    Implement the cone detection using color segmentation algorithm
+    Implement the orange color detection using color segmentation algorithm
     Input:
         img: np.3darray; the input image with a cone to be detected
     Return:
-        bbox: ((x1, y1), (x2, y2)); the bounding box of the cone, unit in px
+        bbox: ((x1, y1), (x2, y2)); the bounding box of the target, unit in px
                 (x1, y1) is the bottom left of the bbox and (x2, y2) is the top right of the bbox
     """
     # Find bounding box coordinates
-    # x1 = 0
-    # y1 = 0
-    # x2 = 0
-    # y2 = 0
+    x1 = 0
+    y1 = 0
+    x2 = 0
+    y2 = 0
     LOW_HUE = 3
     HIGH_HUE = 27
     LOW_SATURATION = 159
     HIGH_SATURATION = 255
     LOW_VALUE = 160
     HIGH_VALUE = 255
-
+    ln_color = (0,255,0)
+    
     cone_img = img.copy()
      
     #convert to hsv
@@ -42,16 +43,22 @@ def cd_color_segmentation(img):
     #get contours
     _, contours, hierarchy = cv2.findContours(total_mask, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
 
-    #get largest contour
-    contour_areas = [cv2.contourArea(contour) for contour in contours]
-    max_ind = np.argmax(contour_areas)
-    biggest_contour = contours[max_ind]
-
-    #draw bounding rectangle
-    x1,y1,w,h = cv2.boundingRect(biggest_contour)
-    x2,y2 = (x1+w, y1+h)
+    if contours:
+        #get largest contour
+        contour_areas = [cv2.contourArea(contour) for contour in contours]
+        max_ind = np.argmax(contour_areas)
+        biggest_contour = contours[max_ind]
+ 
+        #if target area detected is large enough, send firing signal 
+        biggest_contour_area = contour_areas[max_ind]
+        if biggest_contour_area > 10000:
+            ln_color = (0, 0, 255)
+            print("ready to fire")
+        #draw bounding rectangle
+        x1,y1,w,h = cv2.boundingRect(biggest_contour)
+        x2,y2 = (x1+w, y1+h)
     cv2.rectangle(cone_img,(x1,y1),(x2,y2),(0,255,0),2)
     ########### YOUR CODE ENDS HERE ###########
 
     # Return bounding box
-    return ((x1, y1), (x2, y2))
+    return [((x1, y1), (x2, y2)), ln_color]
