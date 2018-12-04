1ffbe0277c742442464ababf2d5fac768126617e
marcusabate <marcus.abate98@gmail.com>
2018-12-03 19:33:48 -0500

    Updated Readme and added Drone class and an example test script.

--- README.md
+++ README.md
@@ -1,6 +1,32 @@
-# 1630DroneShooting
+# CooperativeMambois
+
+*UNDER CONSTRUCTION*
 
 ## DEVEL NOTES
 
 * Always remember to develop on your own test branch before merging to master. Talk to Marcus if you have questions about this.
-* ignore_this.txt was placed in every subdirectory so that the directories would push as empty. Feel free to delete once you have added code to the subdirectory.
+* ``__init__.py`` is added to every subdirectory so that modules defined in those directories can inherit some of the same functionality. Leave that in there for now.
+
+``src/Drone.py`` is a useful tool for quick iteration and testing. You can use this by copying it into the directory you are working in and then calling it as an import in your script:
+
+```
+from Drone import Drone
+```
+
+ at the top of your script. Then, you define a class for your test script that *inherits* the Drone class:
+
+ ```
+ class TestDrone(Drone):
+ ```
+
+ You must then redefine the follwing functions within your class:
+
+ ```
+ def flight_func():
+
+ def vision_cb():
+
+ def sensor_cb():
+ ```
+
+ Note that you only have to redefine the two callbacks if you're planning on using them. ``flight_func`` must be redefined always. See the comments in ``Drone.py`` to get a better idea of how this works, or see ``src/detection_drone_test.py`` for a working example.
---
+++ src/detection_drone_test.py
@@ -0,0 +1,72 @@
+"""
+    detection_drone_test.py
+    Marcus Abate | 16.30
+    12/2/18
+
+    This script tests usage of the generalized DetectionDrone class so that
+    testing and iteration can happen faster.
+"""
+
+from Drone import Drone
+import cv2
+
+class DetectionDroneTest(Drone):
+    def flight_func(self, mamboVision, args):
+        """
+        mambo takes off, flies forward, flies back and lands.
+        """
+        if self.test_flying:
+            print('taking off')
+            self.mambo.safe_takeoff(5)
+
+            if self.mambo.sensors.flying_state != 'emergency':
+
+                # this part is required to prevent faulty readings from contributing to position estimate
+                print("sensor calib")
+                while self.mambo.sensors.speed_ts == 0:
+                    continue
+                self.start_measure = True
+
+                print("flying directly forward")
+                self.mambo.fly_direct(roll=0, pitch=20, yaw=0, vertical_movement=0,
+                                        duration=4)
+                self.mambo.smart_sleep(3)
+
+                print("flying directly backward")
+                self.mambo.fly_direct(roll=0, pitch=-20, yaw=0, vertical_movement=0,
+                                        duration=4)
+
+            print("\n\n\nlanding\n\n\n")
+            self.mambo.safe_land(5)
+
+        else:
+            print("testFlying is False, no takeoff.")
+
+    def sensor_cb(self, args):
+        """
+        print speeds.
+        """
+        print([self.mambo.sensors.speed_x,
+                self.mambo.sensors.speed_y,
+                self.mambo.sensors.speed_z])
+
+    def vision_cb(self, args):
+        """
+        save latest image.
+        """
+        img = self.mamboVision.get_latest_valid_picture()
+
+        if img is not None:
+            cv2.imwrite('last_capture.png', ing)
+        else:
+            print('image is none')
+
+
+test_flying = True
+mambo_addr = "e0:14:ed:d2:3d:d1"
+use_wifi = False
+use_vision = False
+
+if __name__ == "__main__":
+    detection_drone = DetectionDroneTest(test_flying, mambo_addr, use_wifi, use_vision)
+    detection_drone.execute()
