e955f3ee13d32fd57cb35cd60cc686dae3c92fe0
Abigail Lee <abbielee145@gmail.com>
2018-12-06 13:44:13 -0500

    Implement target_dist

--- perception/PerceptionUtils.py
+++ perception/PerceptionUtils.py
@@ -18,3 +18,27 @@ def target_dist(bb_size, plot = False):
     Returns: a float representing estimated distance from target using a
              fitted to the bb_size -> distance data
     """
+
+    meas_dist = np.array([i for i in range(10, 111, 10)])
+    meas_bb = np.array([1272789, 613795, 304475, 206437, 123487, 75603, 63869,
+                        49215, 38115, 33741, 28665])
+
+    # TODO(abbielee): find form of function
+    def func(x, a, b):
+        """
+        Define form of function to fit to data
+        """
+        return (a*x**2)**-1 + b
+
+
+    params = curve_fit(func, meas_bb, meas_dist)
+    print(params)
+    p = np.poly1d(params[0])
+
+    # plot fit
+    if plot:
+        xp = np.linspace(20000, 2000000, 1000)
+        _ = plt.plot(meas_bb, meas_dist, '.', xp, p(xp), '--')
+        plt.show()
+
+    return p(bb_size)
